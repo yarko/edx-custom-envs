89a98844e6e02cf9ee82efbca262d0ac4d9f2c21
yarko <yarkot1@gmail.com>
2014-01-22 01:58:01 -0600

    keep fullstack specifics separate; don't push up .git files;

--- push.envs.sh
+++ push.envs.sh
@@ -20,11 +20,22 @@ fi
 sed -e 's/devstack/aws/' ${CUSTOM}/devstack_custom.py > /tmp/aws_custom.py
 touch -r ${CUSTOM}/devstack_custom.py /tmp/aws_custom.py
 
+##
+# Note: rsync uses the .rsync-filter file
 # the lms
 rsync -avup ${CUSTOM}/devstack_*.py /tmp/aws_*.py ${EDX_PLATFORM}/lms/envs
 # the cms
 rsync -avup ${CUSTOM}/devstack_*.py /tmp/aws_*.py ${EDX_PLATFORM}/cms/envs
 
 # the theme space for devstack, assumed to be one above edx-platform:
-rsync -avup ${CUSTOM}/theme.env.json ${CUSTOM}/themes ${EDXAPP}
+rsync -avup -F ${CUSTOM}/theme.env.json ${CUSTOM}/themes ${EDXAPP}
+
+
+# for fullstack, do more:
+service nginx status 2>&1 | grep -q unrecognized &&
+	exit 0
+
+echo "next, compile assets manually: run the 'rake gather_assets' commands,"
+echo "...see https://github.com/yarko/edx-custom-envs/wiki"
+echo "then run the script 'fullstack.config.sh'"
 
