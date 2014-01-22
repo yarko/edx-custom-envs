8e8af2db7c5b57af066a51aceceeb6ff1c895de9
yarko <yarkot1@gmail.com>
2014-01-21 23:27:53 -0600

    rather than manually create settings vars, create them from the json; be sure to have a themes default

--- devstack_custom.py
+++ devstack_custom.py
@@ -12,8 +12,8 @@ from .devstack import *
 
 FEATURES['USE_CUSTOM_THEME']=True
 
-THEME_DEFAULT = 'stanford'  # A default theme name, or None
 THEME_SETTINGS = 'theme.env.json'
+THEME_NAME = 'stanford'  # A default theme name, from here...
 
 
 # grab settings from the env file;
@@ -22,6 +22,17 @@ THEME_SETTINGS = 'theme.env.json'
 with open(CONFIG_ROOT / THEME_SETTINGS) as env_file:
     ENV_TOKENS = json.load(env_file)
 
+# we'll set the vars this way;
+# - our check is that we assume upper+digit+_ only for keys;
+# - if no var set, and note previously set - that's it.
+set_vars = globals()
+for K in ENV_TOKENS.keys():
+    assert K.isupper()  # accepts digits & '_' as well
+    set_vars[K] = ENV_TOKENS[K]
+
+##
+# Some of the ugly explicits we had before:
+'''
 PLATFORM_NAME = ENV_TOKENS['PLATFORM_NAME']
 SITE_NAME = ENV_TOKENS['SITE_NAME']
 
@@ -35,4 +46,4 @@ BUGS_EMAIL = ENV_TOKENS.get('BUGS_EMAIL', BUGS_EMAIL)
 # Marketing link overrides
 for key, value in ENV_TOKENS.get('MKTG_URL_LINK_MAP', {}).items():
     MKTG_URL_LINK_MAP[key] = value
-
+'''
